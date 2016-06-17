2a9ed1b0d7020a473458c0f8a7873972e23458fc
石瀚 <Shi@shihandemacbook-pro.local>
2016-06-17 14:26:21 +0800

    Initial Commit

---
+++ library_system/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  library_system
+//
+//  Created by 石瀚 on 16/6/17.
+//  Copyright © 2016年 石瀚. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
